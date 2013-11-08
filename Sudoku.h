4d80a919484f1352547ece33164ffbb451556145
padeyemi-tgm <adeyemi.paul@student.tgm.ac.at>
2013-11-08 15:01:52 +0100

    Added .gitignore & header files

---
+++ Sudoku.h
@@ -0,0 +1,14 @@
+//
+//  Sudoku.h
+//  
+//
+//  Created by Paul Adeyemi on 08.11.13.
+//
+//
+
+#ifndef _Sudoku_h
+#define _Sudoku_h
+
+
+
+#endif
---
+++ Sudoku_alg.h
@@ -0,0 +1,14 @@
+//
+//  Sudoku_alg.h
+//  
+//
+//  Created by Paul Adeyemi on 08.11.13.
+//
+//
+
+#ifndef _Sudoku_alg_h
+#define _Sudoku_alg_h
+
+
+
+#endif
