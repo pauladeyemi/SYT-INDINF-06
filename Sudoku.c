63b08dcc1e69496933a5973af0c54e469bfe4af2
AdeyemiPaul <adeyemi.paul@student.tgm.ac.at>
2013-11-08 14:09:13 +0100

    Created Sudoku.c & Sudoku_alg.c

---
+++ Sudoku.c
@@ -0,0 +1,9 @@
+//
+//  Sudoku.c
+//  
+//
+//  Created by Paul Adeyemi on 08.11.13.
+//
+//
+
+#include <stdio.h>
---
+++ Sudoku_alg.c
@@ -0,0 +1,9 @@
+//
+//  Sudoku_alg.c
+//  
+//
+//  Created by Paul Adeyemi on 08.11.13.
+//
+//
+
+#include <stdio.h>
